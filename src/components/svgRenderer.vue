b0e7a189ad56fb5f261f7a5905589d50646afa34
emi <emiliorizzo@gmail.com>
2017-09-22 18:21:13 +0000

    remove duplicates ids

--- src/components/svgRenderer.vue
+++ src/components/svgRenderer.vue
@@ -9,7 +9,7 @@
     )
    
     //-> links  
-    g( v-if='strLinks' id="links" class="links")
+    g.links#l-links( v-if='strLinks')
         line( v-for="link,index in links" 
           :x1='link.source.x' 
           :y1='link.source.y' 
@@ -20,7 +20,7 @@
           :stroke-width='linkWidth'
           :class='linkClass(link.id)')
     
-    g( v-else id="links" class="links" )
+    g.links#l-links(v-else)
         path(v-for="link in links" 
           :d="curve(link)"
           @click='emit("linkClick",[$event,link])'
@@ -28,7 +28,8 @@
           :stroke-width='linkWidth'
           :class='linkClass(link.id) + " curve"')
     //- -> nodes
-    g.nodes#nodes(v-for='(node,key) in nodes')
+    g.nodes#l-nodes
+      template(v-for='(node,key) in nodes')
         svg(v-if='svgIcon(node)' 
           :key='key'
           :viewBox='svgIcon(node).attrs.viewBox'
@@ -62,7 +63,7 @@
         )
 
     //- -> Labels  
-    g.labels( v-if="nodeLabels" id="node-labels")
+    g.labels#node-labels( v-if="nodeLabels")
       text.node-label(v-for="node in nodes"
         :x='node.x + (getNodeSize(node) / 2) + (fontSize / 2)'
         :y='node.y + labelOffset.y'
