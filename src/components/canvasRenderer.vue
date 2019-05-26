d939504500f91e2c85b79a0e7665e4d3aae944e2
Aitor M. Fornies <aitorfornies@hotmail.com>
2019-05-26 21:36:44 +0200

    Zoom, pan and removed hitCanvas for performance

--- src/components/canvasRenderer.vue
+++ src/components/canvasRenderer.vue
@@ -3,10 +3,6 @@
     :width='size.w'
     :height='size.h'
     :style='canvasStyle'
-    @mouseup.prevent='canvasClick'
-    @mousedown.prevent='canvasClick'
-    @touchstart.prevent='canvasClick'
-    @touchend.passive='canvasClick'
     v-render-canvas='{links, nodes}'
     )
 </template>
@@ -32,13 +28,11 @@ export default {
     'labelOffset',
     'canvasStyles',
     'nodeSym',
-    'noNodes'
+    'noNodes',
+    'transform'
   ],
   data () {
     return {
-      hitCanvas: null,
-      shapes: {},
-      drag: null,
       stylesReady: false,
       CssStyles: true, // load style from css props
       // canvas styles
@@ -71,11 +65,7 @@ export default {
     }
   },
   mounted () {
-    let vm = this
-    this.$nextTick(() => {
-      vm.hitCanvas.width = vm.size.w
-      vm.hitCanvas.height = vm.size.h
-    })
+    this.$emit('rendererMounted')
   },
   watch: {
     nodeSize () {
@@ -109,13 +99,6 @@ export default {
     },
     // creates 'virtual' canvas to catch mouse interaction
     canvasInit () {
-      let hitCanvas = document.createElement('canvas')
-      hitCanvas.width = this.size.w
-      hitCanvas.height = this.size.h
-      hitCanvas.top = this.offset.y
-      hitCanvas.left = this.offset.x
-      hitCanvas.id = 'hit-canvas'
-      this.hitCanvas = hitCanvas
       this.resetSprites()
     },
     resetSprites () {
@@ -123,35 +106,6 @@ export default {
       let sprites = ['node', 'nodeSelected', 'nodePinned', 'nodeSelectedPinned']
       for (let sp of sprites) {
         this.sprites[sp] = this.nodeSprite(this.styles[sp])
-      }
-    },
-    // canvas click handler
-    canvasClick (event) {
-      let hitCtx = this.hitCanvas.getContext('2d')
-      let e = (event.touches) ? event.touches[0] || event.changedTouches[0] : event
-      let scrollTop = document.body.scrollTop
-      let scrollLeft = document.body.scrollLeft
-      let x = e.clientX + scrollLeft - this.padding.x
-      let y = e.clientY + scrollTop - this.padding.y
-
-      let pixel = hitCtx.getImageData(x, y, 1, 1).data
-      let color = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`
-      let shape = this.shapes[color]
-      if (shape) {
-        let col = shape.type + 's'
-        let item = this[col][shape.index]
-        if (item) {
-          if (event.type === 'mouseup' || event.type === 'touchend') {
-            if (this.drag) {
-              this.drag = null
-              this.emit('dragEnd')
-            }
-            this.emit(shape.type + 'Click', [event, item])
-          } else if (event.type === 'mousedown' || event.type === 'touchstart') {
-            this.drag = item
-            this.emit('dragStart', [event, item.index])
-          }
-        }
       }
     },
     // draw circ node to canvas
@@ -190,9 +144,7 @@ export default {
     },
     // render canvas
     draw (nodes, links, canvas) {
-      if (!this.hitCanvas) this.canvasInit()
       let ctx = canvas.getContext('2d')
-      let hitCtx = this.hitCanvas.getContext('2d')
       if (!this.stylesReady && this.CssStyles) {
         this.getCssStyles()
         this.resetSprites()
@@ -200,54 +152,53 @@ export default {
       // clean canvas
       ctx.clearRect(0, 0, this.size.w, this.size.h)
 
+      ctx.save()
+      // Transform (zoom / pan)
+      if (this.transform) {
+        ctx.translate(this.transform.x, this.transform.y)
+        ctx.scale(this.transform.k, this.transform.k)
+      }
+
       // draw  links
       ctx = this.setCtx(ctx, this.styles.link)
       for (let link of links) {
         if (!this.linksSelected[link.id]) {
           this.drawLink(ctx, link)
         }
-        this.mapShape(link, 'link', this.drawLink, hitCtx)
       }
 
       // draw selected links
       ctx = this.setCtx(ctx, this.styles.linkSelected)
       for (let lid in this.linksSelected) {
         let link = this.linksSelected[lid]
-
-        if (this.isOnView(link.source) && this.isOnView(link.target)) {
-          this.drawLink(ctx, link)
-        }
+        this.drawLink(ctx, link)
       }
 
       // draw nodes
       ctx = this.setCtx(ctx, this.styles.node)
       for (let node of nodes) {
-        if (this.isOnView(node)) {
-          if (!this.noNodes) {
-            let sprite = this.getNodeSprite(node)
-            ctx.drawImage(sprite, node.x - sprite.width / 2, node.y - sprite.height / 2)
-            // map node shape
-            this.mapShape(node, 'node', this.drawNode, hitCtx)
-          }
-          // draw  node labels
-          if (this.nodeLabels) {
-            node.size = this.nodeSize
-            this.drawText(node, ctx, this.labelStyle(node))
-            // ctx = this.setCtx(ctx, this.styles.node)
-          }
+        if (!this.noNodes) {
+          let sprite = this.getNodeSprite(node)
+          ctx.drawImage(sprite, node.x - sprite.width / 2, node.y - sprite.height / 2)
+        }
+        // draw  node labels
+        if (this.nodeLabels) {
+          node.size = this.nodeSize
+          this.drawText(node, ctx, this.labelStyle(node))
+          // ctx = this.setCtx(ctx, this.styles.node)
         }
       }
       // draw selected nodes
       ctx = this.setCtx(ctx, this.styles.nodeSelected)
       for (let nid in this.selected) {
         let node = this.selected[nid]
-        if (this.isOnView(node)) {
-          // this.drawNode(node, ctx)
-          // let sprite = this.sprites.nodeSelected
-          let sprite = this.getNodeSprite(node)
-          ctx.drawImage(sprite, node.x - sprite.width / 2, node.y - sprite.height / 2)
-        }
+        // this.drawNode(node, ctx)
+        // let sprite = this.sprites.nodeSelected
+        let sprite = this.getNodeSprite(node)
+        ctx.drawImage(sprite, node.x - sprite.width / 2, node.y - sprite.height / 2)
       }
+
+      ctx.restore()
     },
     getNodeSprite (node) {
       let name = this.nodeSpriteName(node)
@@ -298,58 +249,6 @@ export default {
       canvas.width = size
       canvas.height = size
       return canvas
-    },
-    isOnView (obj) {
-      return (obj.x > 0 && obj.y > 0 && obj.x < this.size.w && obj.y < this.size.h)
-    },
-    // index shapes by random colors
-    mapShape (shape, type, drawFunc, hitCtx) {
-      // search unique color index
-      if (!shape.colorIndex) shape.colorIndex = this.newColorIndex()
-      let nShape = Object.assign({}, shape)
-      nShape.color = shape.colorIndex.rgb
-      nShape.borderColor = shape.colorIndex.rgb
-      nShape.type = type
-      // uncoment to debug
-      // hitCtx = this.$refs.canvas.getContext('2d') // to debug
-      let sprite = this.sprites[type] // the color is not important
-      if (sprite) {
-        sprite = this.cloneCanvas(sprite)
-        sprite = this.fillCanvas(sprite, shape.colorIndex)
-        hitCtx.drawImage(sprite, shape.x - sprite.width / 2, shape.y - sprite.height / 2)
-      } else {
-        drawFunc(hitCtx, nShape)
-      }
-      this.shapes[shape.colorIndex.rgb] = nShape
-    },
-    fillCanvas (canvas, color) {
-      let ctx = canvas.getContext('2d')
-      let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-      let pixels = canvas.width * canvas.height * 4
-      for (let p = 3; p <= pixels; p += 4) {
-        if (imgData.data[p] > 0) { // a
-          imgData.data[p] = 255 // sers alpha max to prevent transparency match
-          imgData.data[p - 3] = color.r // r
-          imgData.data[p - 2] = color.g // g
-          imgData.data[p - 1] = color.b // b
-        }
-      }
-      ctx.putImageData(imgData, 0, 0)
-      return canvas
-    },
-    // generates color intex to shapes
-    newColorIndex () {
-      while (true) {
-        let color = this.randomColor()
-        if (!this.shapes[color.rgb]) return color
-      }
-    },
-    // generates random color
-    randomColor () {
-      const r = Math.round(Math.random() * 255)
-      const g = Math.round(Math.random() * 255)
-      const b = Math.round(Math.random() * 255)
-      return { r, g, b, rgb: `rgb(${r},${g},${b})` }
     },
     // sets canvas context style
     setCtx (ctx, conf) {
